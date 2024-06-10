e8a70feb947acc3f57bf419ae997259aac4228b6
mbahnunung <mbahnunung@users.noreply.github.com>
2024-06-10 16:52:46 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,33 @@
+/*
+|--------------------------------------------------------------------------
+| Configuration
+|--------------------------------------------------------------------------
+|
+| To configure your streaming, define the name of your radio, url and 
+| type of your streaming below.
+|
+*/
+
+var settings = {
+    'radio_name': 'Bintang Tenggara',
+
+    // URL of streaming
+    'url_streaming': 'https://asv.alhastream.com/listen/radio_bintang_tenggara_2.0/radio',
+
+    //NOW PLAYING AzuraCast API.
+    'api_url': 'https://asv.alhastream.com/api/nowplaying/radio_bintang_tenggara_2.0',
+
+    // Visit https://api.vagalume.com.br/docs/ to get your API key
+    'api_key': '18fe07917957c289983464588aabddfb',
+
+    // Set to true to get the last songs played
+    'historic': true,
+
+    // Only for SHOUTCast!
+    // Set to true to show the next song to be played. 
+    // Make sure your streaming show this information!
+    'next_song': false,
+
+    // Path to default cover art when none are found
+    'default_cover_art': 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiIUmU3WDb_IzhuaFgQeTvWR35SQGpCrCIN4ifCrJ9-mYB83BtcnBMjUikMZNKx1kOiYxP6O_c3iDdt6UNSC-pgqY6TJ0lupqxTYVZraREwa-FGkUJAEEBg7ZkG4ixvSIWmn0nnQhmr0_2yN8Y3fACkxqbb1ILSveD21F6R3nWRrIrsUECI5lFulqrb-O3U/s1600/m60X6pG.png',
+};
