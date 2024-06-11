d8448d07b2b78b8ab9cffe8533c60f8fad78bbaf
mbahnunung <mbahnunung@users.noreply.github.com>
2024-06-11 12:06:45 +0700

    Update kn.js

--- js/kn.js
+++ js/kn.js
@@ -34,7 +34,7 @@ window.onload = function () {
   // Interval to get streaming data in miliseconds
   setInterval(function () {
     getStreamingData();
-  }, 10);
+  }, 10000);
 
   var coverArt = document.getElementsByClassName('cover-album')[0];
 
