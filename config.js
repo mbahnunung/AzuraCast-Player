7a0bcf8c0ada6d1f11a21b1c51a22b241a069b30
mbahnunung <mbahnunung@users.noreply.github.com>
2024-07-01 23:19:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ var settings = {
     'url_streaming': 'https://asv.alhastream.com/listen/radio_bintang_tenggara_2.0/radio',
 
     //NOW PLAYING AzuraCast API.
-    'api_url': 'https://asv.alhastream.com/api/nowplaying/38',
+    'api_url': 'https://asv.alhastream.com/api/nowplaying/34',
 
     // Visit https://api.vagalume.com.br/docs/ to get your API key
     'api_key': '18fe07917957c289983464588aabddfb',
