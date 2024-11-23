d41a6de4f8db691b60a8d6908968d5606319a076
mbahnunung <mbahnunung@users.noreply.github.com>
2024-11-23 18:55:53 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -120,7 +120,7 @@ function Page() {
             if (this.readyState === 4 && this.status === 200) {
                 var data = JSON.parse(this.responseText);
                 var artworkUrl100 = data.results;
-                var urlCoverArt = artworkUrl100.artwork.medium;
+                var urlCoverArt = artworkUrl100.artwork.big;
 
                 coverArt.style.backgroundImage = 'url(' + urlCoverArt + ')';
                 coverArt.className = 'animated bounceInLeft';
